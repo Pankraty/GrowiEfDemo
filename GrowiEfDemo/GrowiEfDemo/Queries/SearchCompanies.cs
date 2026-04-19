0b368e3def6eb6b3148f01ec0987e0fa44a1a989
a.pankratev <a.pankratev@tbank.ru>
2026-04-19 10:49:47 +0400

    Implement search with exact match

---
+++ GrowiEfDemo/GrowiEfDemo/Queries/SearchCompanies.cs
@@ -0,0 +1,52 @@
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrowiEfDemo.Queries;
+
+public static class SearchCompanies
+{
+    public record Request(
+        string? Name,
+        string? Inn,
+        string? Ogrn,
+        int Limit = 50,
+        int Offset = 0);
+
+    public record Response(int TotalCount, CompanyModel[] Companies);
+    public record CompanyModel(int Id, string Name, string Inn, string Ogrn);
+
+    public class Handler(DemoDbContext dbContext) : Endpoint<Request, Response>
+    {
+        public override void Configure()
+        {
+            Get("/companies");
+            AllowAnonymous();
+        }
+        
+        public override async Task HandleAsync(Request request, CancellationToken ct)
+        {
+            var query = dbContext.Companies.AsNoTracking();
+            if (!string.IsNullOrEmpty(request.Name))
+            {
+                query = query.Where(c => c.Name == request.Name);
+            }
+            if (!string.IsNullOrEmpty(request.Inn))
+            {
+                query = query.Where(c => c.Inn == request.Inn);
+            }
+            if (!string.IsNullOrEmpty(request.Ogrn))
+            {
+                query = query.Where(c => c.Ogrn == request.Ogrn);
+            }
+            var totalCount = await query.CountAsync(ct);
+            var companies = await query.OrderBy(c => c.Id)
+                .Skip(request.Offset)
+                .Take(request.Limit)
+                .Select(c => new CompanyModel(c.Id, c.Name, c.Inn, c.Ogrn))
+                .ToArrayAsync(ct);
+            
+            var response = new Response(totalCount, companies);
+            await Send.ResponseAsync(response, cancellation: ct);
+        }
+    }
+}
