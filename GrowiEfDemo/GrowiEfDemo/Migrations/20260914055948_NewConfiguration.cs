2d64e17e63d51216fc13fe7cd7bf0b873a3d27d4
a.pankratev <a.pankratev@tbank.ru>
2026-09-14 10:00:23 +0400

    Create migration

---
+++ GrowiEfDemo/GrowiEfDemo/Migrations/20260914055948_NewConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrowiEfDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("create or replace function websearch_to_prefixed_tsquery(query text)\r\nreturns tsquery\r\nimmutable\r\nreturn replace(\r\n    websearch_to_tsquery('simple', query)::text || ' ',\r\n    ''' ',\r\n    ''':*'\r\n  )::tsquery;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop function websearch_to_prefixed_tsquery(text);");
+        }
+    }
+}
